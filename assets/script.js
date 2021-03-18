86199d9380908c448e0e683b213297997bc89196
Wyatt Simmons <simmonsw182@gmail.com>
2021-03-18 02:21:18 -0700

    created planner layout

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+var $container = $('.container');
+
 // Get current day 
 var now = moment().format('dddd, MMMM Do');
 console.log(now);
@@ -6,4 +8,66 @@ console.log(now);
 var $currentDay = $('#currentDay');
 $currentDay.text(now);
 
+// hourly planner build
+for (var i = 0; i <= 8; i++) {
+  var hour = i + 9;
+
+  var showHour = 0;
+  var ampm = '';
+  if (hour > 12) {
+    showHour = hour - 12;
+    ampm = 'pm';
+  } else {
+    showHour = hour;
+    ampm = 'am';
+  }
+
+  // create rows
+  var $row = $('<div>');
+  $row.addClass('row');
+
+  // create timeCol
+  var $timeCol = $('<div>');
+  $timeCol.addClass('col-2');
+
+  // create timeBlocks
+  var $hour = $('<div>');
+  $hour.addClass('hour col-1 d-flex justify-content-end align-items-center');
+  var $timeBlock = $('<div>');
+  $timeBlock.addClass('timeBlock text-center');
+  $timeBlock.text(showHour + ampm);
+
+  // append
+  $row.append($hour);
+  $hour.append($timeBlock);
+
+  // create description
+  var $description = $('<div>');
+  $description.addClass('description past col-10');
+
+  // create textarea
+  var $textarea = $('textarea');
+
+  // append
+  $row.append($description);
+  $description.append($textarea);
+
+  // create saveBtn
+  var $saveBtn = $('<div>');
+  var $saveSpan = $('<span>');
+  $saveSpan.text('Save');
+
+  $saveBtn.addClass('saveBtn col-1 text-center d-flex justify-content-center align-items-center');
+  
+
+  // append
+  $row.append($saveBtn);
+  $saveBtn.append($saveSpan);
+
+  // append row to container
+  $container.append($row);
+  
+  console.log(showHour, ampm);
+  console.log(i);
+}
 
